d27ca32c3b8b7da8948adffeef0f570e92137269
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-02-04 17:35:57 +0100

    Bump to 0.1.0

--- setup.py
+++ setup.py
@@ -10,11 +10,15 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+# Read version from VERSION file
+with open(path.join(here, 'VERSION')) as version_file:
+    version = version_file.read().strip()
+
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 setup(
     name='srv6-sdn-data-plane',  
-    version='1.0-beta',
+    version=version,
     description='SRv6 SDN Data Plane',  # Required
     long_description=long_description,
     long_description_content_type='text/markdown',  # Optional (see note above)
